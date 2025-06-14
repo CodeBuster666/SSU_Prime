afddac9f3d448cf1a0af01416ed01dc274950959
Ongskie33 <edwardjohn.ong@ssu.edu.ph>
2025-06-14 01:28:44 -0700

    Margielyn

---
+++ lib/trial/marg.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Marg extends StatelessWidget {
+    const Marg ({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    
+  }
+}
